a4c59d026fb6b13379ff408b7f627d8376612d02
yellhtutaung <yellhtut4@gmail.com>
2022-10-27 12:06:07 +0600

    create user migration for users

--- migration/create_users.js
+++ migration/create_users.js
@@ -23,3 +23,27 @@ var usersSchema = new mongoose.Schema({
 var dd=mongoose.model('buzzy_posts',usersSchema);
 
 module.exports = dd;
+
+// Meet Database Structure ( MongoDb )
+// ___________________________________
+//
+//
+// Table Name ( users )
+//
+//
+// 1  id
+// 2  name_id
+// 3   balance
+// 4  name
+// 5  gender
+// 6  date_of_birth
+// 7  role ( admin , vip , user ) // vip is purchase user
+// 8  email
+// 9  password
+// 10 phone
+// 11 profile
+// 12 ban
+// 13 status
+// 14 token
+// 15 created_at
+// 16 updated_at
