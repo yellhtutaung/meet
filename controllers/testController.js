dea6a404e165cf1a80b96fb8e28b1d9eba3539b9
yellhtutaung <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>
2023-01-26 00:16:42 +0630

    async await issue

--- controllers/testController.js
+++ controllers/testController.js
@@ -13,18 +13,21 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-let generateImage = async () =>
+let generateImage = async (want) =>
 {
     const openai = new OpenAIApi(configuration);
     const response = await openai.createImage({
-        prompt: "shwedagon",
+        prompt: want,
         n: 2,
         size: "1024x1024",
+    }).then(response => {
+        if (response.status == 200)
+        {
+            image_url = response.data.data[0].url;
+            console.log(image_url);
+            return image_url;
+        }
     });
-    console.log('++++++++++++++++++++++++++');
-    image_url = response.data.data[0].url;
-    console.log(image_url);
-    return image_url;
 }
 
 module.exports = {aa,generateImage};
--- route/testRoute.js
+++ route/testRoute.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const testController = require('../controllers/testController');
+const {json} = require("express");
 
 router.get('/',(req,res)=>
 {
-    res.render(`test`,{outData:testController.generateImage()});
+    // let resByOpenAI = testController.generateImage('nature');
+    res.render(`test`,{resByOpenAI:'resByOpenAI'});
+});
+
+router.post('/', async (req,res)=>
+{
+    // console.log(req.body.inputVal);
+    return await testController.generateImage(req.body.inputVal);
+    // .then(response => {
+    //         console.log(response);
+            // return res.json({code:200,resByOpenAI:response});
+    // });
+
+    // res.render(`test`,{resByOpenAI:resByOpenAI});
 });
 
 module.exports = router;
